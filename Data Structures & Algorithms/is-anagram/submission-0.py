56fd768b324b1c7f057f9995199d59f32f5ddbc5
Lemon <40273015+Lemon-personal@users.noreply.github.com>
2026-09-03 23:42:43 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sMap = {}
+        tMap = {}
+
+        for i in range(len(s)):
+            if s[i] in sMap:
+                sMap[s[i]] = sMap[s[i]] + 1
+            else:
+                sMap[s[i]] = 1
+
+        for i in range(len(t)):
+            if t[i] in tMap:
+                tMap[t[i]] = tMap[t[i]] + 1
+            else:
+                tMap[t[i]] = 1
+
+        if sMap == tMap:
+            return True
+        
+        return False
